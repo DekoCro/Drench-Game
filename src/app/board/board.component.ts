a9ed4ad3e4b242a5a2f6714c0047c09783867fd7
Andelko <aldeko96@gmail.com>
2019-06-05 14:01:09 +0200

    Board generated with random colors

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -9,59 +9,15 @@ import { RandomService } from '../random.service';
 })
 export class BoardComponent implements OnInit {
 
-  unit: string = '27px';
-  x: number = 14;
-  y: number = 14;
-  numOfFields: any = this.x * this.y;
-  fields: number[] = [];
-  blockColor: string;
-  random: number;
-  lero: number[] = [];
-
   constructor(private service: RandomService) {}
 
-  generateBoard() {
-    for(let i = 0; i < this.numOfFields; i++) {
-      this.fields.push(i);
-    }
-  }
-  gen() {
-    for(let i = 0; i < 196; i++) {
-      let brandom = Math.floor(Math.random() * 6) + 1;
-      this.lero.push(brandom);
-    }
-  }
-
-  shuffleColors() {
-    for(let i = 0; i < this.numOfFields; i++) {
-      this.random = Math.floor(Math.random() * 6) + 1;
-    }
-    switch(this.random) {
-      case 1:
-        this.blockColor = COLORS[0];
-        break;
-      case 2:
-        this.blockColor = COLORS[1];
-        break;
-      case 3:
-        this.blockColor = COLORS[2];
-        break;
-      case 4:
-        this.blockColor = COLORS[3];
-        break;
-      case 5:
-        this.blockColor = COLORS[4];
-        break;
-      case 6:
-        this.blockColor = COLORS[5];
-        break;
-      }
-  }
+  randomColors: string[] = this.service.randomColors;
 
   ngOnInit() {
-    this.generateBoard();
-    this.shuffleColors();
-    this.gen();
-    console.log(this.lero);
+    this.service.generateNumbers();
+    this.service.generateBoard();
+    
+    console.log(this.randomColors);
+    console.log(this.service.randomNumbers);
   }
 }
--- src/app/random.service.ts
+++ src/app/random.service.ts
@@ -6,15 +6,44 @@ import { COLORS } from './colors';
 })
 export class RandomService {
 
+  unit: string = '27px';
+  x: number = 14;
+  y: number = 14;
+  numOfFields: number = this.x * this.y;
   allColors = COLORS;
-  andom: number;
   randomNumbers: number[] = [];
+  randomColors: string[] = [];
 
-  shuffleColors() {
-    for(let i = 0; i < 196; i++) {
+  generateNumbers() {
+    for(let i = 0; i < this.numOfFields; i++) {
       let number = Math.floor(Math.random() * 6) + 1;
       this.randomNumbers.push(number);
     }
+  }
+
+  generateBoard() {
+    this.randomNumbers.forEach(i => {
+      switch(i) {
+        case 1:
+          this.randomColors.push(COLORS[0])
+          break;
+        case 2:
+          this.randomColors.push(COLORS[1])
+          break;
+        case 3:
+          this.randomColors.push(COLORS[2])
+          break;
+        case 4:
+          this.randomColors.push(COLORS[3])
+          break;
+        case 5:
+          this.randomColors.push(COLORS[4])
+          break;
+        case 6:
+          this.randomColors.push(COLORS[5])
+          break;
+        }
+    })
   }
 
   constructor() { }
