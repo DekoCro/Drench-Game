5193288d1b1fc7991d761a18ba7617ac1d6df068
Andelko <aldeko96@gmail.com>
2019-06-21 10:52:21 +0200

    Color interval added if game finsihed

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,3 +1,4 @@
+import { COLORS } from './../colors';
 import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { RandomService } from '../random.service';
 
@@ -19,8 +20,9 @@ export class BoardComponent implements OnInit {
   mainField;
   drenchFields: any[] = [];
   mixedFields: any[] = [];
+  arr: string[] = [];
 
-  play() {
+  play(color) {
     for(let i = 0; i < this.drenchFields.length; i++) {
 
       let drenchX: number = this.service.parsePosition(this.drenchFields[i].style.backgroundPositionX);
@@ -28,17 +30,17 @@ export class BoardComponent implements OnInit {
 
       console.log(this.mixedFields.length - this.drenchFields.length);
 
-      if(this.service.clickedColor !== undefined) {
+      if(color !== undefined) {
 
         for(let i = 0; i <= this.mixedFields.length - 1; i++) {
 
           let mixedX: number = this.service.parsePosition(this.mixedFields[i].style.backgroundPositionX);
           let mixedY: number = this.service.parsePosition(this.mixedFields[i].style.backgroundPositionY);
 
-          let condition1 = drenchX + 1 === mixedX && drenchY === mixedY && this.mixedFields[i].style.backgroundColor === this.service.clickedColor;
-          let condition2 = drenchX  === mixedX && drenchY + 1 === mixedY && this.mixedFields[i].style.backgroundColor === this.service.clickedColor;
-          let condition3 = drenchX - 1 === mixedX && drenchY === mixedY && this.mixedFields[i].style.backgroundColor === this.service.clickedColor;
-          let condition4 = drenchX === mixedX && drenchY - 1 === mixedY && this.mixedFields[i].style.backgroundColor === this.service.clickedColor;
+          let condition1 = drenchX + 1 === mixedX && drenchY === mixedY && this.mixedFields[i].style.backgroundColor === color;
+          let condition2 = drenchX  === mixedX && drenchY + 1 === mixedY && this.mixedFields[i].style.backgroundColor === color;
+          let condition3 = drenchX - 1 === mixedX && drenchY === mixedY && this.mixedFields[i].style.backgroundColor === color;
+          let condition4 = drenchX === mixedX && drenchY - 1 === mixedY && this.mixedFields[i].style.backgroundColor === color;
           let conditionalArray: boolean[] = [condition1, condition2, condition3, condition4];
   
           for(let c in conditionalArray){
@@ -48,17 +50,21 @@ export class BoardComponent implements OnInit {
 
               // This loop changes color of drenched fields to clicked color.
               for(let j = 0; j < this.drenchFields.length; j++) {
-                this.drenchFields[j].style.backgroundColor = this.service.clickedColor;
+                this.drenchFields[j].style.backgroundColor = color;
               }
               // Using indexOf we detect the element that we drenched and then we splice it from array
               let index = this.mixedFields.indexOf(this.mixedFields[i])
               if (index > - 1) {
                 this.mixedFields.splice(index, 1);
-              }
-              // If all the fields are drenched, game completed
-              if(this.mixedFields === []) {
-                for(let o = 0; o < this.drenchFields.length; o++) {
-                  this.drenchFields[o].style.backgroundColor = "black";
+
+                // If all the fields are drenched, change board colors
+                if(this.mixedFields.length === 0){
+                  let n = 0;
+                    setInterval(() => {
+                    let flash = this.randomColors[n]
+                    n = n + 1 % this.randomColors.length;
+                    this.drenchFields.map(field => field.style.backgroundColor = flash)
+                  }, 200);
                 }
               }
             }
@@ -70,8 +76,6 @@ export class BoardComponent implements OnInit {
 
   ngOnInit() {
     this.service.generateBoard();
-    this.play();
-    // TODO: on BoardComponent Init we need to detect elements around our main field to push them into same array straight away
   }
 
   ngAfterViewInit(){
@@ -81,5 +85,10 @@ export class BoardComponent implements OnInit {
 
     this.divs.forEach(div => this.mixedFields.push(div.nativeElement));
     this.mixedFields.shift();
+
+    for(let i = 0; i < this.drenchFields.length; i++) {
+      this.play(this.drenchFields[i].style.backgroundColor)
+    }
+    console.log(this.drenchFields);
   }
 }
